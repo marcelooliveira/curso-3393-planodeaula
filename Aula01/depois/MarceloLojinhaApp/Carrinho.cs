a2b201d4c4f2e1d7d28e9052aa84cc142872ec81
marcelooliveira <mclricardo@gmail.com>
2023-09-17 21:25:05 -0300

    Função Carrinho com GET e POST

--- Aula01/depois/MarceloLojinhaApp/Carrinho.cs
+++ Aula01/depois/MarceloLojinhaApp/Carrinho.cs
@@ -19,11 +19,27 @@ namespace MarceloLojinhaApp
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            HttpResponseData response;
 
-            response.WriteString("[{\"ProductId\":4,\"Icon\":\"🍊\",\"Description\":\"Tangerina (kg)\",\"UnitPrice\":3.50,\"Quantity\":1,\"Total\":3.50,\"Id\":3},{\"ProductId\":13,\"Icon\":\"🍒\",\"Description\":\"Cereja (kg)\",\"UnitPrice\":3.50,\"Quantity\":3,\"Total\":10.50,\"Id\":2},{\"ProductId\":17,\"Icon\":\"\U0001f965\",\"Description\":\"Coco (un)\",\"UnitPrice\":4.50,\"Quantity\":2,\"Total\":9.00,\"Id\":1}]");
+            if (req.Method == "GET")
+            {
+                response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json; charset=utf-8");
 
+                response.WriteString("[{\"ProductId\":4,\"Icon\":\"🍊\",\"Description\":\"Tangerina (kg)\",\"UnitPrice\":3.50,\"Quantity\":1,\"Total\":3.50,\"Id\":3},{\"ProductId\":13,\"Icon\":\"🍒\",\"Description\":\"Cereja (kg)\",\"UnitPrice\":3.50,\"Quantity\":3,\"Total\":10.50,\"Id\":2},{\"ProductId\":17,\"Icon\":\"\U0001f965\",\"Description\":\"Coco (un)\",\"UnitPrice\":4.50,\"Quantity\":2,\"Total\":9.00,\"Id\":1}]");
+
+                return response;
+            }
+
+            if (req.Method == "POST")
+            {
+                response = req.CreateResponse(HttpStatusCode.OK);
+                response.WriteString("ISTO É UM POST");
+                return response;
+            }
+
+            response = req.CreateResponse(HttpStatusCode.BadRequest);
+            response.WriteString("Tipo Http inválido");
             return response;
         }
     }
