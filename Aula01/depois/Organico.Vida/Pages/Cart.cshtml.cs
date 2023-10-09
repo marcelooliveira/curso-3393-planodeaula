fb5ced00649ee5e948d003e1cdbebccfb532921e
marcelooliveira <mclricardo@gmail.com>
2023-10-09 00:47:22 -0300

    organico.vida depois aula 1

---
+++ Aula01/depois/Organico.Vida/Pages/Cart.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Organico.Vida.Pages;
+
+public class CartModel : PageModel
+{
+    private readonly ILogger<CartModel> _logger;
+
+    // HttpClient lifecycle management best practices:
+    // https://learn.microsoft.com/dotnet/fundamentals/networking/http/httpclient-guidelines#recommended-use
+    private static HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri("https://marcelolojinhaapp.azurewebsites.net"),
+    };
+
+    public List<CartItem> CartItems { get; set; }
+
+    public CartModel(ILogger<CartModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnGet()
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync("api/carrinho");
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        CartItems = JsonConvert.DeserializeObject<List<CartItem>>(jsonResponse)!;
+    }
+
+    public IActionResult OnPost()
+    {
+        if (Request.Form.Keys.Contains("addToCartSubmit"))
+        {
+            return Redirect("/addToCart");
+        }
+
+        if (Request.Form.Keys.Contains("checkoutSubmit"))
+        {
+            ECommerceData.Instance.CheckOut();
+        }
+
+        return Redirect("/cart");
+    }
+}
