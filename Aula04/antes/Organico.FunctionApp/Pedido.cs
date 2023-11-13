0febe4e54c6827aa77f9a49c3cef4ec09239bfc9
marcelooliveira <mclricardo@gmail.com>
2023-11-12 23:32:04 -0300

    .

--- Aula04/antes/Organico.FunctionApp/Pedido.cs
+++ Aula04/antes/Organico.FunctionApp/Pedido.cs
@@ -18,7 +18,7 @@ namespace Organico.FunctionApp
         }
 
         [Function("Pedido")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
