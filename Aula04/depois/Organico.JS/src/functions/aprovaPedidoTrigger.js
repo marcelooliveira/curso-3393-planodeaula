3ff31904aa9784a7a8742a01fd4b20d731a0ba40
marcelooliveira <mclricardo@gmail.com>
2023-11-27 23:01:30 -0300

    3393-azure-functions-video4.2-implementando-aprovacao-de-pedidos

--- Aula04/depois/Organico.JS/src/functions/aprovaPedidoTrigger.js
+++ Aula04/depois/Organico.JS/src/functions/aprovaPedidoTrigger.js
@@ -1,9 +1,23 @@
 const { app } = require('@azure/functions');
+const PedidosCosmosClient = require('../../pedidosCosmosClient');
 
 app.storageQueue('aprovaPedidoTrigger', {
     queueName: 'fila-aprova-pedido',
     connection: 'organicofunctionapp20231_STORAGE',
-    handler: (queueItem, context) => {
-        context.log('Storage queue function processed work item:', queueItem);
+    handler: async (queueItem, context) => {
+        const pedidosCosmosClient = new PedidosCosmosClient();
+
+        await pedidosCosmosClient.initializeAsync();
+        const pedido = await pedidosCosmosClient.get(queueItem);
+
+        if (!pedido) {
+            context.error('Pedido não encontado!', queueItem);
+            return;
+        }
+
+        pedido.status = 2; //AGUARDANDO ENTREGA
+        await pedidosCosmosClient.post(pedido);
+
+        context.log('Pedido aprovado com sucesso!', queueItem);
     }
 });
