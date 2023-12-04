7049ca1d607c2b89428fe9e9edffefdfbfa5f2aa
marcelooliveira <mclricardo@gmail.com>
2023-12-03 23:08:58 -0300

    3393-azure-functions-video5.2-implementando-geracao-de-faturamento.mov

--- Aula05/depois/Organico.JS/src/functions/faturamentoTrigger.js
+++ Aula05/depois/Organico.JS/src/functions/faturamentoTrigger.js
@@ -1,8 +1,14 @@
 const { app } = require('@azure/functions');
+const FaturamentoCosmosClient = require('../../faturamentoCosmosClient');
 
 app.timer('faturamentoTrigger', {
     schedule: '0 0 * * * *',
-    handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
+    handler: async (myTimer, context) => {
+        const faturamentoCosmosClient = new FaturamentoCosmosClient();
+
+        await faturamentoCosmosClient.initializeAsync();
+        await faturamentoCosmosClient.gerarFaturamentoMensal();
+
+        context.log('Faturamento mensal gerado com sucesso!');
     }
 });
